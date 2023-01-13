a77091f83fab6e563e7aaef91c961c531661a31e
rcraig12 <rcraig12@gmail.com>
2023-01-13 16:13:29 +0000

    building tests for token

--- test/addLiquidity.js
+++ test/addLiquidity.js
@@ -1,13 +1,44 @@
+const web3 = require('web3');
+const BN = web3.utils.BN;
+
+const chai = require("chai");
+const chaiBN = require('chai-bn')(BN);
+chai.use(chaiBN);
+
+const chaiAsPromised = require('chai-as-promised');
+chai.use(chaiAsPromised);
+
+const expect = chai.expect;
+
 const ElCartelElite = artifacts.require("ElCartelElite");
 
-contract("ElCartelElite", function(accounts){
+contract("ElCartelElite", async() => {
 
-  it("ElCartelElate has 100000000 tokens", function(){
-    return ElCartelElite.deployed().then(function(instance){
-      return instance.balance.call(accounts[0]);
-    }).then(function(balance){
-      assert.equal(balance.valueOf(), 100000000, "Wrong number of tokens!")
-    });
+  it("Deploying contract ElCartelElate", async () => {
+    const elCartelElite = await ElCartelElite.deployed();
+    console.log(elCartelElite.address);
+    assert(elCartelElite.address !== '')
   });
 
-})
+  it("Total Tokens is 1000000000", async()=>{
+    const elCartelElite = await ElCartelElite.deployed();
+    const expectedTotal = new BN('1000000000000000000');
+    const actualTotal = await elCartelElite.totalSupply.call();
+    await expect(actualTotal).to.be.a.bignumber.equal(expectedTotal);
+  });
+
+  it("Max Transaction : 0.003% ", async()=>{
+    const elCartelElite = await ElCartelElite.deployed();
+    const expectedTotal = new BN('1000000000000000000');
+    const actualTotal = await elCartelElite.totalSupply.call();
+    await expect(actualTotal).to.be.a.bignumber.equal(expectedTotal);
+  });
+
+  it("Max Wallet : 0.003%", async()=>{
+    const elCartelElite = await ElCartelElite.deployed();
+    const expectedTotal = new BN('1000000000000000000');
+    const actualTotal = await elCartelElite.totalSupply.call();
+    await expect(actualTotal).to.be.a.bignumber.equal(expectedTotal);
+  });
+
+});
